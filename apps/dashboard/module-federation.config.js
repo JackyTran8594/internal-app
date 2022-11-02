7e0714dedb06a1feb3ee8fab6be66af486674f7c
BACHTUVU\PC <bachtuvu.dev@gmail.com>
2022-11-02 14:24:49 +0700

    update

--- apps/dashboard/module-federation.config.js
+++ apps/dashboard/module-federation.config.js
@@ -1,6 +1,13 @@
+
 module.exports = {
   name: 'dashboard',
   exposes: {
     './Module': 'apps/dashboard/src/app/remote-entry/entry.module.ts',
   },
+  output: {
+    uniqueName: "dashboard",
+    publicPath: "auto",
+    scriptType: "text/javascript"
+  },
+ 
 };
--- apps/task-management/module-federation.config.js
+++ apps/task-management/module-federation.config.js
@@ -1,6 +1,13 @@
+
 module.exports = {
   name: 'task-management',
   exposes: {
     './Module': 'apps/task-management/src/app/remote-entry/entry.module.ts',
   },
+  output: {
+    uniqueName: "task-management",
+    publicPath: "auto",
+    scriptType: "text/javascript"
+  },
+ 
 };
