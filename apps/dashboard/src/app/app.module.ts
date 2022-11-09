3675c07eea8aefef10fdb6bfe994ca18614e9fc0
dhthanh1997 <74946734+dhthanh1997@users.noreply.github.com>
2022-11-09 17:46:48 +0700

    09/11/2022 Thành update Dashboard

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
   declarations: [AppComponent, MainComponent],
   imports: [
     BrowserModule,
+    NzIconModule,
     RouterModule.forRoot(
       [
         {
--- apps/dashboard/src/app/main/main.module.ts
+++ apps/dashboard/src/app/main/main.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NzIconModule } from 'ng-zorro-antd/icon';
 import { MainRoutingModule } from './main-routing.module';
-
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    MainRoutingModule
-  ]
+  imports: [CommonModule, MainRoutingModule, NzIconModule],
 })
-export class MainModule { }
+export class MainModule {}
